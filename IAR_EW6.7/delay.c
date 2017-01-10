0029d918cc0120e3403334f605869ca94b186b61
Cristi Ilea <ileacristian85@gmail.com>
2017-01-10 17:21:27 +0200

    Modify delay module so that it can use any timer using #define TIMER_COUNTER

--- IAR_EW6.7/delay.c
+++ IAR_EW6.7/delay.c
@@ -3,27 +3,28 @@
 #include "board.h"
 #include "types.h"
 
+#define TIMER_COUNTER (TIM16->CNT)
+
 /* variables for delay_us function witch is blocking and has only one instance */
-static volatile u16 t15_tstamp, t15_delta;
+static volatile u16 timer_tstamp, timer_delta;
 
 /* variables for delay_us_nonblocking functions witch are nonblocking and have their own variables */
-static volatile u16 t15nonblock_tstamp1, t15nonblock_delta1;
-static u8 t15nonblock_step1 = 0;
-static volatile u16 t15nonblock_tstamp2, t15nonblock_delta2;
-static u8 t15nonblock_step2 = 0;
+static volatile u16 timernonblock_tstamp1, timernonblock_delta1;
+static u8 timernonblock_step1 = 0;
+static volatile u16 timernonblock_tstamp2, timernonblock_delta2;
+static u8 timernonblock_step2 = 0;
 /**
-  Uses Timer 15
   * @brief  time delay in us unit
   * @param  n_us is how many us to delay
   * @retval None
   */
 void delay_us(u16 n_us)
 {
-  t15_tstamp = TIM15->CNT;
+  timer_tstamp = TIMER_COUNTER;
   while(1)
   {
-    t15_delta = TIM15->CNT - t15_tstamp;
-    if(t15_delta >= n_us) break;
+    timer_delta = TIMER_COUNTER - timer_tstamp;
+    if(timer_delta >= n_us) break;
   }
 }
 
@@ -35,16 +36,16 @@ void delay_us(u16 n_us)
   */
 u8 delay_us_nonblocking1(u16 n_us)
 {
-  if(t15nonblock_step1 == 0)
+  if(timernonblock_step1 == 0)
   {
-    t15nonblock_tstamp1 = TIM15->CNT;
-    t15nonblock_step1 = 1;
+    timernonblock_tstamp1 = TIMER_COUNTER;
+    timernonblock_step1 = 1;
   }
   else
   {
-    t15nonblock_delta1 = TIM15->CNT - t15nonblock_tstamp1;
-    if(t15nonblock_delta1 >= n_us) {
-      t15nonblock_step1 = 0;
+    timernonblock_delta1 = TIMER_COUNTER - timernonblock_tstamp1;
+    if(timernonblock_delta1 >= n_us) {
+      timernonblock_step1 = 0;
       return 1;
     }
   }
@@ -53,16 +54,16 @@ u8 delay_us_nonblocking1(u16 n_us)
 
 u8 delay_us_nonblocking2(u16 n_us)
 {
-  if(t15nonblock_step2 == 0)
+  if(timernonblock_step2 == 0)
   {
-    t15nonblock_tstamp2 = TIM15->CNT;
-    t15nonblock_step2 = 1;
+    timernonblock_tstamp2 = TIMER_COUNTER;
+    timernonblock_step2 = 1;
   }
   else
   {
-    t15nonblock_delta2 = TIM15->CNT - t15nonblock_tstamp2;
-    if(t15nonblock_delta2 >= n_us) {
-      t15nonblock_step2 = 0;
+    timernonblock_delta2 = TIMER_COUNTER - timernonblock_tstamp2;
+    if(timernonblock_delta2 >= n_us) {
+      timernonblock_step2 = 0;
       return 1;
     }
   }
