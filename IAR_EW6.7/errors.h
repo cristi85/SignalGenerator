291b82ede8cbb6894f292ecae350d4e537f15b30
Cristi Ilea <ileacristian85@gmail.com>
2017-02-15 18:14:47 +0200

    Add Clock errors

--- IAR_EW6.7/errors.h
+++ IAR_EW6.7/errors.h
@@ -3,10 +3,11 @@
 
 #include "types.h"
 
-#define ERROR_ADC_INIT   (u8)0
-#define ERROR_ADS1112    (u8)1
+#define ERROR_ADS1112    (u8)0
+#define ERROR_CLOCK_SRC  (u8)1
+#define ERROR_CLOCK_FREQ (u8)2
 
-#define ERRORS (u8)ERROR_ADS1112 + 1
+#define ERRORS (u8)ERROR_CLOCK_FREQ + 1
 
 void Errors_Init(void);
 void Errors_SetError(u8);
