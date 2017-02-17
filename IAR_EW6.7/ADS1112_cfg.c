8e4352e0ab6f3796c35db6917d0a99a5a55d9333
Cristi Ilea <ileacristian85@gmail.com>
2017-02-17 17:34:18 +0200

    Add configurable timeout to I2C communication based on free running timer TIM2 (32bit)

--- IAR_EW6.7/ADS1112_cfg.c
+++ IAR_EW6.7/ADS1112_cfg.c
@@ -12,18 +12,19 @@
 u8 I2C_WriteBytesNoSlaveReg(u8* databuff, u8 numbytes, u8 slave_adr)
 {
   u8 i;
+  u32 l_tstamp;
   
-  Timeout_SetTimeout1(100);
-  while(I2C_GetFlagStatus(I2C2, I2C_FLAG_BUSY) == SET && !Timeout_IsTimeout1());
-  if(Timeout_IsTimeout1()) return 0;
+  l_tstamp = TIMER_1US_CNT;
+  while(I2C_GetFlagStatus(I2C2, I2C_FLAG_BUSY) == SET && ((TIMER_1US_CNT - l_tstamp) < I2C_COMM_TOUT_US));
+  if((TIMER_1US_CNT - l_tstamp) >= I2C_COMM_TOUT_US) return 0;
   
   /* Configure slave address, nbytes, reload, end mode and start or stop generation */
   I2C_TransferHandling(I2C2, ADS1112_ADR, numbytes, I2C_AutoEnd_Mode, I2C_Generate_Start_Write);
   
   //TXIS is set after Address and ACK bit is transmitted
-  Timeout_SetTimeout1(100);
-  while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXIS) == RESET && !Timeout_IsTimeout1());
-  if(Timeout_IsTimeout1()) return 0;
+  l_tstamp = TIMER_1US_CNT;
+  while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXIS) == RESET && ((TIMER_1US_CNT - l_tstamp) < I2C_COMM_TOUT_US));
+  if((TIMER_1US_CNT - l_tstamp) >= I2C_COMM_TOUT_US) return 0;
   
   if(I2C_GetFlagStatus(I2C2, I2C_FLAG_NACKF) == SET)
   {
@@ -34,9 +35,9 @@ u8 I2C_WriteBytesNoSlaveReg(u8* databuff, u8 numbytes, u8 slave_adr)
   for(i = 0; i < numbytes; i++)
   {
     I2C_SendData(I2C2, databuff[i]);
-    Timeout_SetTimeout1(100);
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE) == RESET && !Timeout_IsTimeout1());
-    if(Timeout_IsTimeout1()) return 0;
+    l_tstamp = TIMER_1US_CNT;
+    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_TXE) == RESET && ((TIMER_1US_CNT - l_tstamp) < I2C_COMM_TOUT_US));
+    if((TIMER_1US_CNT - l_tstamp) >= I2C_COMM_TOUT_US) return 0;
     if(I2C_GetFlagStatus(I2C2, I2C_FLAG_NACKF) == SET)
     {
       return 0;  // data not acknowledged by slave
@@ -45,9 +46,9 @@ u8 I2C_WriteBytesNoSlaveReg(u8* databuff, u8 numbytes, u8 slave_adr)
   
   //Wait for the stop flag to be set indicating
   //a stop condition has been sent
-  Timeout_SetTimeout1(100);
-  while(I2C_GetFlagStatus(I2C2, I2C_FLAG_STOPF) == RESET && !Timeout_IsTimeout1());
-  if(Timeout_IsTimeout1()) return 0;
+  l_tstamp = TIMER_1US_CNT;
+  while(I2C_GetFlagStatus(I2C2, I2C_FLAG_STOPF) == RESET && ((TIMER_1US_CNT - l_tstamp) < I2C_COMM_TOUT_US));
+  if((TIMER_1US_CNT - l_tstamp) >= I2C_COMM_TOUT_US) return 0;
   
   //Clear the stop flag for the next potential transfer
   I2C_ClearFlag(I2C2, I2C_FLAG_STOPF);
@@ -64,10 +65,11 @@ u8 I2C_WriteBytesNoSlaveReg(u8* databuff, u8 numbytes, u8 slave_adr)
 u8 I2C_ReadBytesNoSlaveReg(u8* data, u8 numbytes, u8 slave_adr)
 {
   u8 i;
+  u32 l_tstamp;
   
-  Timeout_SetTimeout1(100);
-  while(I2C_GetFlagStatus(I2C2, I2C_FLAG_BUSY) == SET && !Timeout_IsTimeout1());
-  if(Timeout_IsTimeout1()) return 0;
+  l_tstamp = TIMER_1US_CNT;
+  while(I2C_GetFlagStatus(I2C2, I2C_FLAG_BUSY) == SET && ((TIMER_1US_CNT - l_tstamp) < I2C_COMM_TOUT_US));
+  if((TIMER_1US_CNT - l_tstamp) >= I2C_COMM_TOUT_US) return 0;
   
   I2C_TransferHandling(I2C2, ADS1112_ADR, numbytes, I2C_AutoEnd_Mode, I2C_Generate_Start_Read);
   
@@ -80,9 +82,9 @@ u8 I2C_ReadBytesNoSlaveReg(u8* data, u8 numbytes, u8 slave_adr)
   
   for(i = 0; i < numbytes; i++)
   {
-    Timeout_SetTimeout1(100);
-    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_RXNE) == RESET && !Timeout_IsTimeout1());
-    if(Timeout_IsTimeout1()) return 0;
+    l_tstamp = TIMER_1US_CNT;
+    while(I2C_GetFlagStatus(I2C2, I2C_FLAG_RXNE) == RESET && ((TIMER_1US_CNT - l_tstamp) < I2C_COMM_TOUT_US));
+    if((TIMER_1US_CNT - l_tstamp) >= I2C_COMM_TOUT_US) return 0;
     data[i] = I2C_ReceiveData(I2C2);
   }
   return 1;
--- IAR_EW6.7/ADS1112_cfg.h
+++ IAR_EW6.7/ADS1112_cfg.h
@@ -6,6 +6,9 @@
 
 #define ADS1112_ADR (u8)0x90   /* A1:A0 tied to GND */
 
+#define TIMER_1US_CNT     (TIM2->CNT)
+#define I2C_COMM_TOUT_US  (u32)(1000)   
+
 u8 I2C_ReadBytesNoSlaveReg(u8*, u8, u8);
 u8 I2C_WriteBytesNoSlaveReg(u8* databuff, u8 numbytes, u8 slave_adr);
 
